605b692ab594235a4cb9409b2ba02897f8237c81
Eduardo Tioma <tioma.eduardo@gmail.com>
2019-05-25 10:10:36 -0300

    Adicionado FormClosing event para fechar a porta COM e desligar o led quando o programa fechar

--- ledDEMO/Form1.Designer.cs
+++ ledDEMO/Form1.Designer.cs
@@ -29,12 +29,71 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.ledButton = new System.Windows.Forms.Button();
+            this.comPortsCombobox = new System.Windows.Forms.ComboBox();
+            this.arduinoConnectButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ledButton
+            // 
+            this.ledButton.Location = new System.Drawing.Point(110, 22);
+            this.ledButton.Margin = new System.Windows.Forms.Padding(2);
+            this.ledButton.Name = "ledButton";
+            this.ledButton.Size = new System.Drawing.Size(122, 64);
+            this.ledButton.TabIndex = 0;
+            this.ledButton.Text = "LIGA LED";
+            this.ledButton.UseVisualStyleBackColor = true;
+            this.ledButton.Click += new System.EventHandler(this.ledButton_Click);
+            // 
+            // comPortsCombobox
+            // 
+            this.comPortsCombobox.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comPortsCombobox.FormattingEnabled = true;
+            this.comPortsCombobox.Location = new System.Drawing.Point(100, 110);
+            this.comPortsCombobox.Margin = new System.Windows.Forms.Padding(2);
+            this.comPortsCombobox.Name = "comPortsCombobox";
+            this.comPortsCombobox.Size = new System.Drawing.Size(92, 29);
+            this.comPortsCombobox.TabIndex = 1;
+            // 
+            // arduinoConnectButton
+            // 
+            this.arduinoConnectButton.Location = new System.Drawing.Point(195, 105);
+            this.arduinoConnectButton.Margin = new System.Windows.Forms.Padding(2);
+            this.arduinoConnectButton.Name = "arduinoConnectButton";
+            this.arduinoConnectButton.Size = new System.Drawing.Size(64, 34);
+            this.arduinoConnectButton.TabIndex = 2;
+            this.arduinoConnectButton.Text = "Conectar";
+            this.arduinoConnectButton.UseVisualStyleBackColor = true;
+            this.arduinoConnectButton.Click += new System.EventHandler(this.arduinoConnectButton_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(331, 158);
+            this.Controls.Add(this.arduinoConnectButton);
+            this.Controls.Add(this.comPortsCombobox);
+            this.Controls.Add(this.ledButton);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ledButton;
+        private System.Windows.Forms.ComboBox comPortsCombobox;
+        private System.Windows.Forms.Button arduinoConnectButton;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- ledDEMO/Form1.cs
+++ ledDEMO/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,75 @@ namespace ledDEMO
 {
     public partial class Form1 : Form
     {
+        List<string> comPortsList = new List<string>();
+        SerialPort serialPort = new SerialPort();
+        const int baudRate = 9600;
+        bool IsLedOn = false;
+          
         public Form1()
         {
             InitializeComponent();
+            timer.Enabled = true;
+            ledButton.Enabled = false;
+            
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            foreach (var port in SerialPort.GetPortNames())
+            {
+                if (!comPortsCombobox.Items.Contains(port))
+                {
+                    comPortsCombobox.Items.Add(port);
+                }
+            }
+        }
+
+        private void arduinoConnectButton_Click(object sender, EventArgs e)
+        {
+            if(!serialPort.IsOpen) //serialport.IsOpen == false
+            {
+                try
+                {
+                    string portName = comPortsCombobox.Text;
+                    serialPort = new SerialPort(portName, baudRate);
+                    serialPort.Open();
+                    MessageBox.Show("SUCESSO", "Arduino conectado!");
+                    timer.Stop();
+                    ledButton.Enabled = true;
+                }
+                catch(IOException exception)
+                {
+                    MessageBox.Show("ERRO", exception.Message);
+                }
+            }
+        }
+
+        private void ledButton_Click(object sender, EventArgs e)
+        {
+            if (!IsLedOn)
+            {
+                serialPort.Write("1");
+                ledButton.Text = "DESLIGA LED";
+                IsLedOn = true;
+            }
+            else if (IsLedOn)
+            {
+                serialPort.Write("0");
+                ledButton.Text = "LIGA LED";
+                IsLedOn = false;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort.IsOpen == true)
+            {
+                serialPort.Write("0");
+                serialPort.Close();
+            }
         }
     }
+
+        
 }
